ee010822854ada639000bdde08a7148b8d5d40d4
github username <github email address>
2018-01-03 11:37:57 +0200

    enabled sessions and set session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
 
   get "/" do
