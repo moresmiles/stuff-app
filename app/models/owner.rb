491d19d63f56213087197953297c5322932a2daf
github username <github email address>
2018-01-14 12:14:25 +0200

    owner class has_secure_password and validations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,11 @@ class Owner < ActiveRecord::Base
   has_many :moves
   has_many :boxes, through: :moves
   has_many :items, through: :boxes
+
+  has_secure_password
+
+  EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\Z/i
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+
 end
