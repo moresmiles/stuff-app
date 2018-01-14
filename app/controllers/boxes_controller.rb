6df8206367db70185c89c00c8acd31b8a85b369e
github username <github email address>
2018-01-14 12:09:48 +0200

    boxes controller and view

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,2 +1,11 @@
 class BoxesController < ApplicationController
+
+  get '/moves/:id/boxes' do
+    if logged_in?
+      @move = current_move
+      erb :'/boxes/boxes'
+    else
+      redirect_if_not_logged_in
+    end
+  end
 end
