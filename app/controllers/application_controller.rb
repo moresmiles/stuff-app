a7e18970f1dd8a10dcde018b1b05ebaf4998da67
github username <github email address>
2018-01-14 11:53:09 +0200

    made index route and erb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,24 @@
 require './config/environment'
 require 'rack-flash'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
 
   use Rack::Flash
+use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions unless test?
+    enable :sessions
     set :session_secret, "secret"
   end
 
   get "/" do
+  get '/' do
+    erb :index
+  end
     erb :welcome
   end
 
