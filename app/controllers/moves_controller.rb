0f72233d259192bb64f107587a204cf9dca73a30
github username <github email address>
2018-01-14 12:04:10 +0200

    all (previous) moves route and view

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,2 +1,11 @@
 class MovesController < ApplicationController
+
+  get '/moves' do
+    if logged_in?
+      @moves = current_user.moves.all
+      erb :'/moves/all_moves'
+    else
+      redirect_if_not_logged_in
+    end
+  end
 end
