752e388e5358f0f394435c3a8e060760d96b4146
github username <github email address>
2018-01-14 12:05:48 +0200

    moves edit controllers and view, includes delete route

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -27,4 +27,38 @@ class MovesController < ApplicationController
       redirect to "/moves/#{@move.id}"
     end
   end
+
+  get '/moves/:id/edit' do
+    if logged_in?
+     erb :'/moves/edit'
+   else
+     redirect_if_not_logged_in
+   end
+  end
+
+  patch '/moves/:id' do
+   if !params[:origin].empty? && !params[:destination].empty? && !params[:move_date].empty?
+     @move = current_move
+     @move.update(origin: params[:origin], destination: params[:destination], move_date: params[:move_date])
+     flash[:message] = "Move Updated"
+     redirect to "/moves"
+   else
+     flash[:message] = "Please don't leave blank content"
+     redirect to "/moves/#{params[:id]}/edit"
+   end
+  end
+
+  delete '/moves/:id/delete' do
+   if logged_in?
+     #flash message -are you sure you want to delete this move?
+     #all associated boxes and items will be deleted as well
+     @move = current_move
+     @move.destroy
+     redirect to "/moves"
+   else
+     redirect_if_not_logged_in
+   end
+ end
+
+
 end
