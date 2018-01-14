8a94e0b70e39bca5eec5453c09cf856cb0a20846
github username <github email address>
2018-01-14 12:10:33 +0200

    new box routes and view

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -8,4 +8,28 @@ class BoxesController < ApplicationController
       redirect_if_not_logged_in
     end
   end
+
+  get '/moves/:id/boxes/new' do
+    if logged_in?
+      @move = current_move
+      erb :'boxes/new'
+    else
+      redirect_if_not_logged_in
+    end
+  end
+
+  post '/moves/:id/boxes' do
+    if params[:name].empty? || params[:location].empty?
+      flash[:alert] = "Pleae don't leave blank content"
+      redirect to '/moves/:id/boxes/new'
+    else
+      @owner = current_user
+      @move = current_move
+      @box = Box.create(owner_id: @owner.id, move_id: @move.id, name: params[:name], location: params[:location])
+      @move.boxes << @box
+      @move.save
+      @owner.save
+      redirect to "/moves/boxes/#{@box.id}"
+    end
+  end
 end
