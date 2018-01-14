3c24f062597df706eeee3a27555275103abeb31f
github username <github email address>
2018-01-14 12:13:23 +0200

    items new controllers and view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,29 @@ class ItemsController < ApplicationController
       redirect_if_not_logged_in
     end
   end
+
+  get '/boxes/:id/items/new' do
+    if logged_in?
+      @box = Box.find(params[:id])
+      erb :'/items/new'
+    else
+      redirect_if_not_logged_in
+    end
+  end
+
+  post '/boxes/:id/items' do
+    if !params[:name].empty?
+      @owner = current_user
+      @box = Box.find(params[:id])
+      @move = @box.move_id
+      @item = Item.create(box_id: @box.id, move_id: @move, owner_id: @owner.id, name: params[:name])
+      @box.items << @item
+      @box.save
+      @owner.save
+      redirect to "/moves/boxes/#{@box.id}"
+    else
+      flash[:alert] = "Pleae don't leave blank content"
+      redirect to '/moves/:id/items/new'
+    end
+  end
 end
