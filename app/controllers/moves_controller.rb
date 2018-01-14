b30adb0adf498fe74393745532915bf395c368b5
github username <github email address>
2018-01-14 12:04:56 +0200

    new move routes and view

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -8,4 +8,23 @@ class MovesController < ApplicationController
       redirect_if_not_logged_in
     end
   end
+
+  get '/moves/new' do
+    redirect_if_not_logged_in
+    erb :'/moves/new'
+  end
+
+  post '/moves' do
+    redirect_if_not_logged_in
+    if params[:origin].empty? || params[:destination].empty? || params[:move_date].empty?
+      flash[:alert] = "Pleae don't leave blank content"
+      redirect to '/moves/new'
+    else
+      @owner = current_user
+      @move = Move.create(owner_id: @owner.id, origin: params[:origin], destination: params[:destination], move_date: params[:move_date])
+      @owner.moves << @move
+      @owner.save
+      redirect to "/moves/#{@move.id}"
+    end
+  end
 end
