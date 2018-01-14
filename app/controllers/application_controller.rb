c717e7cb1a857b3897d47e90f42d6bdc6c32b2e7
github username <github email address>
2018-01-14 12:02:32 +0200

    helper methods logged_in, current_user, current_move, and redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,34 @@ use Rack::Flash
     erb :welcome
   end
 
+  helpers do
+
+   def logged_in?
+     !!session[:user_id]
+   end
+
+   def current_user
+     Owner.find(session[:user_id])
+   end
+
+   def current_move
+     current_user.moves.find_by(params[:id])
+   end
+
+   def redirect_if_not_logged_in
+     if !logged_in?
+       redirect "/login"
+     end
+   end
+
+   def redirect_to_index_page
+     redirect to "/"
+   end
+
+   def redirect_to_owner_page
+     redirect to "/owners/:id"
+   end
+ end
+
+
 end
