733ff52175aa22f66a95dd8cfebb0ddf0bd86401
github username <github email address>
2018-01-14 12:12:55 +0200

    item show controller and view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+
+  get '/boxes/items/:id' do
+    if logged_in?
+      @box = Box.find(params[:id])
+      @item = Item.find(params[:id])
+      erb :'/items/show'
+    else
+      redirect_if_not_logged_in
+    end
+  end
 end
