33a1eb5d1289f08498c4531465d1d4767eb884cb
github username <github email address>
2018-01-25 13:50:29 +0200

    user authentication for moves controller added

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,21 +1,35 @@
 class MovesController < ApplicationController
 
-  get '/moves' do
-    if logged_in?
-      @moves = current_user.moves.all
-      erb :'/moves/all_moves'
+  get '/moves/:id' do
+    if !logged_in?
+      redirect_to_login_page
     else
-      redirect_if_not_logged_in
+      @owner = Owner.find(params[:id])
+        if @owner == current_user
+          @moves = current_user.moves.all
+          erb :'/moves/all_moves'
+      else
+        flash[:alert] = "Please only access your move page"
+        redirect to "moves/#{current_user.id}"
+      end
     end
   end
 
-  get '/moves/new' do
-    redirect_if_not_logged_in
-    erb :'/moves/new'
+  get '/moves/:id/new' do
+    if !logged_in?
+      redirect_to_login_page
+    else
+      @owner = Owner.find(params[:id])
+        if @owner == current_user
+          erb :'/moves/new'
+      else
+        flash[:alert] = "Please only access your new move page"
+        redirect to "moves/#{current_user.id}/new"
+      end
+    end
   end
 
   post '/moves' do
-    redirect_if_not_logged_in
     if params[:origin].empty? || params[:destination].empty? || params[:move_date].empty?
       flash[:alert] = "Pleae don't leave blank content"
       redirect to '/moves/new'
@@ -24,41 +38,54 @@ class MovesController < ApplicationController
       @move = Move.create(owner_id: @owner.id, origin: params[:origin], destination: params[:destination], move_date: params[:move_date])
       @owner.moves << @move
       @owner.save
-      redirect to "/moves"
+      redirect to "/moves/#{@owner.id}"
     end
   end
 
   get '/moves/:id/edit' do
-    if logged_in?
-     erb :'/moves/edit'
-   else
-     redirect_if_not_logged_in
-   end
+    if !logged_in?
+      redirect_to_login_page
+    else
+      @move = Move.find(params[:id])
+      if @move == current_move
+        erb :'/moves/edit'
+    else
+      flash[:alert] = "Please only edit your page"
+        redirect to "moves/#{current_move.id}/edit"
+      end
+    end
   end
 
   patch '/moves/:id' do
-   if !params[:origin].empty? && !params[:destination].empty? && !params[:move_date].empty?
-     @move = current_move
-     @move.update(origin: params[:origin], destination: params[:destination], move_date: params[:move_date])
-     flash[:message] = "Move Updated"
-     redirect to "/moves"
-   else
+    if params[:origin].empty? || params[:destination].empty? || params[:move_date].empty?
      flash[:message] = "Please don't leave blank content"
      redirect to "/moves/#{params[:id]}/edit"
-   end
+   else
+     @move = Move.find(params[:id])
+     if @move.owner == current_user
+        @move.update(origin: params[:origin], destination: params[:destination], move_date: params[:move_date])
+        flash[:message] = "Move Updated"
+        redirect to "/moves#{@move.owner.id}"
+      else
+        flash[:alert] = "Please only edit your page"
+        redirect_to_index_page
+      end
+    end
   end
 
   delete '/moves/:id/delete' do
-   if logged_in?
-     #flash message -are you sure you want to delete this move?
-     #all associated boxes and items will be deleted as well
-     @move = current_move
-     @move.destroy
-     redirect to "/moves"
+   if !logged_in?
+     redirect_to_login_page
    else
-     redirect_if_not_logged_in
-   end
- end
-
+     @move = Move.find(params[:id])
+      if @move.owner == current_user
+        @move.destroy
+        redirect to "/moves/#{current_user.id}"
+      else
+        flash[:alert] = "Please only edit your page"
+        redirect to "/moves/#{current_user.id}"
+      end
+    end
+  end
 
 end
