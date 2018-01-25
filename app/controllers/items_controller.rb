bf61e8ba900f72a125f7a1da6e6b0c880dc7d7f6
github username <github email address>
2018-01-25 13:51:11 +0200

    user authentication for items controller added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,69 +1,95 @@
 class ItemsController < ApplicationController
 
   get '/boxes/items/:id' do
-    if logged_in?
-      @box = Box.find(params[:id])
+    if !logged_in?
+      redirect_to_login_page
+    else
       @item = Item.find(params[:id])
+      if @item.box.move.owner == current_user
       erb :'/items/show'
     else
-      redirect_if_not_logged_in
+      flash[:alert] = "Please only access your pages"
+      redirect to "moves/boxes/#{current_box.id}"
     end
   end
+end
 
   get '/boxes/:id/items/new' do
-    if logged_in?
-      @box = Box.find(params[:id])
-      erb :'/items/new'
+    if !logged_in?
+      redirect_to_login_page
     else
-      redirect_if_not_logged_in
+      @box = Box.find(params[:id])
+      if @box.move.owner == current_user
+        erb :'/items/new'
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect to "boxes/#{current_box.id}/items/new"
+      end
     end
   end
 
   post '/boxes/:id/items' do
-    if !params[:name].empty?
-      @owner = current_user
-      @box = Box.find(params[:id])
-      @move = @box.move_id
-      @item = Item.create(box_id: @box.id, move_id: @move, owner_id: @owner.id, name: params[:name])
-      @box.items << @item
-      @box.save
-      @owner.save
-      flash[:message] = "Item Created"
-      redirect to "/moves/boxes/#{@box.id}"
-    else
+    if params[:name].empty?
       flash[:alert] = "Pleae don't leave blank content"
-      redirect to '/moves/:id/items/new'
+      redirect to "/boxes/#{params[:id]}/items/new"
+    else
+      @box = Box.find(params[:id])
+      if @box.move.owner == current_user
+        @item = Item.create(box_id: @box.id, name: params[:name])
+        @box.items << @item
+        @box.move.owner.save
+        flash[:message] = "Item Created"
+        redirect to "/moves/boxes/#{@box.id}"
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect to "boxes/#{current_box.id}/items/new"
+      end
     end
   end
 
   get '/boxes/items/:id/edit' do
-    if logged_in?
-      @item = Item.find(params[:id])
-      erb :'items/edit'
+    if !logged_in?
+      redirect_to_login_page
     else
-      redirect_if_not_logged_in
+      @item = Item.find(params[:id])
+      if @item.box.move.owner == current_user
+      erb :'items/edit'
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect to "/moves/boxes/#{current_box.id}"
+      end
     end
   end
 
   patch '/boxes/items/:id' do
-    if !params[:name].empty?
-      @item = Item.find(params[:id])
-      @item.update(name: params[:name])
-      flash[:message] = "Item Updated"
-      redirect to "/boxes/items/#{@item.id}"
-    else
+    if params[:name].empty?
       flash[:message] = "Please don't leave blank field"
-      redirect to "/boxes/items/#{@item.id}/edit"
+      redirect to "/boxes/items/#{params[:id]}/edit"
+    else
+      @item = Item.find(params[:id])
+      if @item.box.move.owner == current_user
+        @item.update(name: params[:name])
+        flash[:message] = "Item Updated"
+        redirect to "/boxes/items/#{@item.id}"
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect to "/moves/boxes/#{current_box.id}"
+      end
     end
   end
 
   delete '/boxes/items/:id/delete' do
-    if logged_in?
-      @item = Item.find(params[:id])
-      @item.delete
-      redirect to "/moves/boxes/#{@item.box_id}"
+    if !logged_in?
+      redirect_to_login_page
     else
-      redirect_if_not_logged_in
+      @item = Item.find(params[:id])
+      if @item.box.move.owner == current_user
+        @item.delete
+        redirect to "/moves/boxes/#{@item.box_id}"
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect to "/moves/boxes/#{current_box.id}"
+      end
     end
   end
 
