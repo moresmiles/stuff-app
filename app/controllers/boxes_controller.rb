6689ec7e2a602c1f74c74e784cb959d89746aff5
github username <github email address>
2018-01-25 13:50:55 +0200

    user authentication for boxes controller added

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,78 +1,110 @@
 class BoxesController < ApplicationController
 
   get '/moves/:id/boxes' do
-    if logged_in?
-      @move = current_move
+    if !logged_in?
+      redirect_to_login_page
+    else
+      @move = Move.find(params[:id])
+      if @move.owner == current_user
       erb :'/boxes/boxes'
     else
-      redirect_if_not_logged_in
+      flash[:alert] = "Please only access your pages"
+      redirect to "/moves/#{current_move.id}/boxes"
     end
   end
+end
 
   get '/moves/:id/boxes/new' do
-    if logged_in?
-      @move = current_move
-      erb :'boxes/new'
+    if !logged_in?
+      redirect_to_login_page
     else
-      redirect_if_not_logged_in
+      @move = Move.find(params[:id])
+      if @move.owner == current_user
+        erb :'boxes/new'
+    else
+      flash[:alert] = "Please only access your pages"
+      redirect to "/moves/#{current_move.id}/boxes"
     end
   end
+end
 
   post '/moves/:id/boxes' do
     if params[:name].empty? || params[:location].empty?
       flash[:alert] = "Pleae don't leave blank content"
-      redirect to '/moves/:id/boxes/new'
+      redirect to  "/moves/#{params[:id]}/boxes/new"
     else
-      @owner = current_user
-      @move = current_move
-      @box = Box.create(owner_id: @owner.id, move_id: @move.id, name: params[:name], location: params[:location])
-      @move.boxes << @box
-      @move.save
-      @owner.save
+      @move = Move.find(params[:id])
+      if @move == current_move
+        @box = Box.create(move_id: @move.id, name: params[:name], location: params[:location])
+        @move.boxes << @box
+        @move.save
+        current_user.save
       redirect to "/moves/boxes/#{@box.id}"
+    else
+      flash[:alert] = "Please only access your pages"
+      redirect_to_index_page
     end
   end
+end
 
   get '/moves/boxes/:id' do
-    if logged_in?
-      @box = Box.find(params[:id])
-      erb :'boxes/show'
+    if !logged_in?
+      redirect_to_login_page
     else
-      redirect_if_not_logged_in
+      @box = Box.find(params[:id])
+      if @box.move == current_move
+        erb :'boxes/show'
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect to "/moves/boxes#{@box.id}"
+      end
     end
   end
 
   get '/moves/:id/boxes/edit' do
-    if logged_in?
-      @box = Box.find(params[:id])
-      erb :'/boxes/edit'
+    if !logged_in?
+      redirect_to_login_page
     else
-      redirect_if_not_logged_in
+      @box = Box.find(params[:id])
+      if @box.move == current_move
+          erb :'/boxes/edit'
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect to "/moves/boxes#{@box.id}"
+      end
     end
   end
 
   patch '/moves/boxes/:id' do
     if !params[:name].empty? && !params[:location].empty?
-      @box = Box.find(params[:id])
-      @box.update(name: params[:name], location: params[:location])
-      flash[:message] = "Box Updated"
-      redirect to "/moves/boxes/#{@box.id}"
-    else
       flash[:message] = "Please don't leave blank fields"
       redirect to "/moves/#{params[:id]}/boxes/edit"
+    else
+      @box = Box.find(params[:id])
+      if @box.move == current_move
+        @box.update(name: params[:name], location: params[:location])
+        flash[:message] = "Box Updated"
+        redirect to "/moves/boxes/#{@box.id}"
+      else
+        flash[:alert] = "Please only access your pages"
+        redirect_to_index_page
+      end
     end
   end
 
   delete '/moves/:id/boxes/delete' do
-   if logged_in?
-     #flash message -are you sure you want to delete this move?
-     #all associated boxes and items will be deleted as well
-     @box = Box.find(params[:id])
-     @box.destroy
-     redirect to "/moves/#{@box.move_id}/boxes"
+   if !logged_in?
+     redirect_to_login_page
    else
-     redirect_if_not_logged_in
-   end
- end
+     @box = Box.find(params[:id])
+     if @box.move == current_move
+       @box.destroy
+       redirect to "/moves/#{current_move.id}/boxes"
+     else
+      flash[:alert] = "Please only access your pages"
+      redirect_to_index_page
+      end
+    end
+  end
 
 end
